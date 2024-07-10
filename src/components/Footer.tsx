cdef6ea14c77981f061f6407b32ba606164a34e3
William Cobb <william@2wc.com>
2024-07-10 11:41:57 -0400

    updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ const Footer: React.FC = () => {
       <div className="container">
         <p>&copy; 2024 William W Cobb III. All rights reserved.</p>
         <div className="social-icons">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
-          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+          <a href="https://www.facebook.com/BuddyCobb" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+          <a href="https://www.instagram.com/buddycobb/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+          <a href="https://www.linkedin.com/in/williamcobbiii/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
+          <a href="https://www.twitter.com/WilliamCobb" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
         </div>
       </div>
     </footer>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,16 +20,16 @@ const Header: React.FC = () => {
             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
           <div className="ml-auto d-flex align-items-stretch">
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="text-white mr-3">
+            <a href="https://www.facebook.com/BuddyCobb" target="_blank" rel="noopener noreferrer" className="text-white mr-3">
               <i className="fab fa-facebook-f"></i>
             </a>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="text-white mr-3">
+            <a href="https://www.instagram.com/buddycobb/" target="_blank" rel="noopener noreferrer" className="text-white mr-3">
               <i className="fab fa-instagram"></i>
             </a>
-            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="text-white mr-3">
+            <a href="https://www.linkedin.com/in/williamcobbiii/" target="_blank" rel="noopener noreferrer" className="text-white mr-3">
               <i className="fab fa-linkedin-in"></i>
             </a>
-            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="text-white">
+            <a href="https://www.twitter.com/WilliamCobb" target="_blank" rel="noopener noreferrer" className="text-white">
               <i className="fab fa-twitter"></i>
             </a>
           </div>
